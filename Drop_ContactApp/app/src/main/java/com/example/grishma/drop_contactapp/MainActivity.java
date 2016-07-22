88008c969bd87c5d535f27bb5f4bef06d8da2bab
Grishma Ukani <grishma@promact.com>
2016-07-22 15:20:03 +0530

    app

---
+++ Drop_ContactApp/app/src/main/java/com/example/grishma/drop_contactapp/MainActivity.java
@@ -0,0 +1,25 @@
+
+package com.example.grishma.drop_contactapp;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // remove title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
